8a89012e637e14456e817e1ff70ce9c5e8271d4f
vueLee <578603626@qq.com>
2019-03-10 15:41:35 +0800

    first commit

---
+++ .lavas/skeleton.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+import Skeleton from 'C:\\Users\\Administrator\\Desktop\\taro\\pwa-project\\core\\Skeleton.vue';
+
+
+export default new Vue({
+    components: {
+        
+        Skeleton,
+        
+    },
+    template: `
+        <div>
+            
+            <skeleton id="skeleton" style="display:none"/>
+            
+        </div>
+    `
+});
